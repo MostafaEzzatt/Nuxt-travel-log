d013008545646c5f046873c6f1f0420ab8eed335
Mostafa Ezzat <psmostafa@gmail.com>
2025-05-02 02:05:59 +0300

    ✨ add typesafe env (#52)

---
+++ lib/env.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+import tryParseEnv from "./try-parse-env";
+
+const envSchema = z.object({
+  NODE_ENV: z.string(),
+});
+
+export type envSchema = z.infer<typeof envSchema>;
+tryParseEnv(envSchema);
+// eslint-disable-next-line node/no-process-env
+export default envSchema.parse(process.env);
