6ea8942c05e4eaeb4504b4a0dd6d94bb45e123a1
MostafaEzzatt <psmostafa@gmail.com>
2025-05-02 01:50:19 +0300

    :sparkles: add theme toggle

---
+++ components/app/theme-toggle.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get: () => colorMode.value,
+  set: (val) => {
+    colorMode.preference = val ? "dark" : "light";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate">
+    <!-- this hidden checkbox controls the state -->
+    <input v-model="isDark" type="checkbox">
+
+    <!-- sun icon -->
+    <Icon class="swap-on h-10 w-10 fill-current" name="tabler:sun" size="24" />
+
+    <!-- moon icon -->
+    <Icon class="swap-off h-10 w-10 fill-current" name="tabler:moon-filled" size="24" />
+
+  </label>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+  ],
   eslint: {
     config: {
       standalone: false,
@@ -15,4 +19,7 @@ export default defineNuxtConfig({
     ],
   },
   css: ["~/assets/css/main.css"],
+  colorMode: {
+    dataValue: "theme",
+  },
 });
