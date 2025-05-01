f91d0e0ff923262a105dd0a263943c8c77e7cf4d
MostafaEzzatt <psmostafa@gmail.com>
2025-05-02 01:25:34 +0300

    :sparkles: Add Landing Page

--- app.vue
+++ app.vue
@@ -1,8 +1,5 @@
 <template>
-  <button class="btn btn-secondary">
-    Show Map
-    <Icon
-      name="tabler:map"
-    />
-  </button>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
