48c36cb4556b2baab27b8a4d94c56f0a73eb1584
MostafaEzzatt <psmostafa@gmail.com>
2025-05-04 07:33:00 +0300

    :sparkles: init auth setup

---
+++ lib/db/migrations/0002_absent_cassandra_nova.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `locationLog` ADD `user_id` integer NOT NULL REFERENCES user(id);--> statement-breakpoint
+ALTER TABLE `locationLogImage` ADD `user_id` integer NOT NULL REFERENCES user(id);
--- lib/db/schema/locations-log.ts
+++ lib/db/schema/locations-log.ts
@@ -1,6 +1,7 @@
 import { real } from "drizzle-orm/gel-core";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
 import { location } from "./locations";
 
 export const locationLog = sqliteTable("locationLog", {
@@ -12,6 +13,7 @@ export const locationLog = sqliteTable("locationLog", {
   lat: real().notNull(),
   long: real().notNull(),
   locationId: int().notNull().references(() => location.id),
+  userId: int().notNull().references(() => user.id),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 
