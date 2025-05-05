24052c9464b76b24668b9e67db3cfdcef371e26c
MostafaEzzatt <psmostafa@gmail.com>
2025-05-05 15:09:48 +0300

    :sparkles add auth state / sign out

--- components/github-auth-btn.vue
+++ components/github-auth-btn.vue
@@ -3,7 +3,26 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button class="btn btn-accent" :disabled="authStore.loading" @click="authStore.signIn">
+  <div v-if="authStore.user" class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn m-1 flex items-center">
+      {{ authStore.user.name }}
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-6 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/dashboard/signout">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+
+  <button v-else class="btn btn-accent" :disabled="authStore.loading" @click="authStore.signIn">
     Sign In With Github
     <span v-if="authStore.loading" class="loading loading-ring loading-xl" />
     <Icon v-else name="tabler:brand-github" size="24" />
---
+++ pages/dashboard/signout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="w-fit mx-auto mt-12 bg-primary px-3.5 py-1 rounded-xs space-x-2 flex items-center">
+    <span class="loading loading-ring loading-xl" />
+
+    <span class="font-medium flex items-center gap-1">Wait We Signing You Out <Icon name="tabler:mood-sad" size="20" />.</span>
+  </div>
+</template>
