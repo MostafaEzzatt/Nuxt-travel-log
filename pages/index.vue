0a8d71a0e0ec4669ddd7c45a90963264927955aa
Mostafa Ezzat <psmostafa@gmail.com>
2025-05-05 16:35:31 +0300

    Tl 17 (#57)
    * :sparkles add auth state / sign out
    
    * :sparkles: add auth middleware

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4 min-h-96 rounded-2xl">
     <div class="hero-content text-center">
@@ -8,7 +12,11 @@
         <p class="py-6">
           Keep track of your travels and adventures with this simple travel log app. Add locations, photos, and notes to create a digital journal of your journeys.
         </p>
-        <GithubAuthBtn />
+
+        <GithubAuthBtn v-if="!authStore.user" />
+        <NuxtLink v-else to="/dashboard" class="btn btn-accent">
+          Dashboard
+        </NuxtLink>
       </div>
     </div>
   </div>
