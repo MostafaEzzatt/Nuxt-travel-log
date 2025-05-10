eaa128b06cea43dd4505f8d24aab90e78a6764c2
Mostafa Ezzat <psmostafa@gmail.com>
2025-05-10 15:10:54 +0300

    :sparkles: add location page (#60)

--- lib/db/schema/locations.ts
+++ lib/db/schema/locations.ts
@@ -1,5 +1,7 @@
 import { real } from "drizzle-orm/gel-core";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
 
 import { user } from "./auth";
 
@@ -14,4 +16,17 @@ export const location = sqliteTable("location", {
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 
+});
+
+export const InsertLocation = createInsertSchema(location, {
+  name: field => field.min(1).max(100),
+  description: field => field.max(500),
+  lat: z.coerce.number().min(-90).max(90),
+  long: z.coerce.number().min(-180).max(180),
+}).omit({
+  id: true,
+  slug: true,
+  userId: true,
+  createdAt: true,
+  updatedAt: true,
 });
